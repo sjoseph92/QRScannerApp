f7219560a83c65404db76e82b729d541c377ae31
Sam Joseph <sjoseph92@gmail.com>
2019-09-09 11:58:26 -0400

    removed stock App.js produced by RN and replaced with authorization screen

--- Screens/App.js
+++ Screens/App.js
@@ -1,114 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
+  KeyboardAvoidingView,
   Text,
-  StatusBar,
+  Keyboard,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  View,
+  Button,
+  AsyncStorage,
+  Switch,
 } from 'react-native';
+import {styles} from './styles/styles.js';
+//import auth from '@react-native-firebase/auth';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  // loginUser = () => {
+  //   //firebase function for authorizing established users
+  //   const {email, password} = this.state;
+  //   auth()
+  //     .signInWithEmailAndPassword(email, password)
+  //     .then(() => {
+  //       alert('success');
+  //     })
+  //     .catch(error => {
+  //       alert(error);
+  //     });
+  // };
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <KeyboardAvoidingView>
+          <Text style={styles.TitleText}>QRScanner</Text>
+          <TouchableOpacity
+            style={styles.ButtonStyle}
+            activeOpacity={0.3}
+            onPress={() => this.loginUser()}>
+            <Text style={styles.TextStyle}>Continue Without Registering</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.ButtonStyle}
+            activeOpacity={0.3}
+            onPress={() => this.props.navigation.navigate('SignUpScreen')}>
+            <Text style={styles.TextStyle}>Login/Create An Account</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
+}
