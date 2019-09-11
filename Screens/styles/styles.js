c2398b5b88ac0bc69645b5751ad8bfeb9f6efdd0
Sam Joseph <sjoseph92@gmail.com>
2019-09-11 13:53:25 -0400

    implemented necessary styling for mask

--- Screens/styles/styles.js
+++ Screens/styles/styles.js
@@ -81,4 +81,28 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
+  maskOutter: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  maskInner: {
+    width: 300,
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  maskFrame: {
+    backgroundColor: 'rgba(1,1,1,0.6)',
+  },
+  maskRow: {
+    width: '100%',
+  },
+  maskCenter: {
+    flexDirection: 'row',
+  },
 });
