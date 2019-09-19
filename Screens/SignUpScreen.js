e8dcc611f7fd0961e9d61a13ccc7c834937fd319
Sam Joseph <sjoseph92@gmail.com>
2019-09-19 07:05:49 -0400

    added the various text inputs needed for account sign up

--- Screens/SignUpScreen.js
+++ Screens/SignUpScreen.js
@@ -12,13 +12,63 @@ import {styles} from './styles/styles.js';
 import auth from '@react-native-firebase/auth';
 
 export default class SignUpScreen extends Component {
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    }
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
 
-    
+  
+
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <TextInput
+          autoCapitalize="none"
+          style={styles.textInput}
+          placeholder="First Name"
+          onChangeText={firstName => this.setState({firstName})}
+          value={this.state.firstName}
+        />
+        <TextInput
+          autoCapitalize="none"
+          style={styles.textInput}
+          placeholder="Last Name"
+          onChangeText={lastName => this.setState({lastName})}
+          value={this.state.lastName}
+        />
+        <TextInput
+          autoCapitalize="none"
+          style={styles.textInput}
+          placeholder="Email"
+          onChangeText={email => this.setState({email})}
+          value={this.state.email}
+        />
+        <TextInput
+          autoCapitalize="none"
+          secureTextEntry
+          style={styles.textInput}
+          placeholder="Password"
+          onChangeText={password => this.setState({password})}
+          value={this.state.password}
+        />
+        <TextInput
+          autoCapitalize="none"
+          secureTextEntry
+          style={styles.textInput}
+          placeholder="Confirm Password"
+          onChangeText={confirmPassword => this.setState({confirmPassword})}
+          value={this.state.confirmPassword}
+        />
+        <TouchableOpacity
+            style={styles.ButtonStyle}
+            activeOpacity={0.3}
+            onPress={() => this.loginAnonymousUser()}>
+            <Text style={styles.TextStyle}>Create Account</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
 }
