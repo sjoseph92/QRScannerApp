9e26c0481c143b7b6e05cd64e7f54b694ff23a75
Sam Joseph <sjoseph92@gmail.com>
2019-09-19 06:16:18 -0400

    added create account text that on click navigates user to account creation. Also added text inputs so users with accounts can log in

--- Screens/AuthenticationScreen.js
+++ Screens/AuthenticationScreen.js
@@ -12,6 +12,11 @@ import {styles} from './styles/styles.js';
 import auth from '@react-native-firebase/auth';
 
 export default class AuthenticationScreen extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   //firebase function for authenticating anonymous users
   loginAnonymousUser = () => {
     auth()
@@ -29,18 +34,41 @@ export default class AuthenticationScreen extends Component {
       <View style={styles.MainContainer}>
         <KeyboardAvoidingView>
           <Text style={styles.TitleText}>QRScanner</Text>
+          <TextInput
+            autoCapitalize="none"
+            style={styles.textInput}
+            placeholder="email"
+            onChangeText={email => this.setState({email})}
+            value={this.state.email}
+          />
+          <TextInput
+            autoCapitalize="none"
+            secureTextEntry
+            style={styles.textInput}
+            placeholder="password"
+            onChangeText={password => this.setState({password})}
+            value={this.state.password}
+          />
+          <TouchableOpacity
+            style={styles.ButtonStyle}
+            activeOpacity={0.3}
+            onPress={() => this.props.navigation.navigate('SignUpScreen')}>
+            <Text style={styles.TextStyle}>Login</Text>
+          </TouchableOpacity>
           <TouchableOpacity
             style={styles.ButtonStyle}
             activeOpacity={0.3}
             onPress={() => this.loginAnonymousUser()}>
             <Text style={styles.TextStyle}>Continue Without Registering</Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.ButtonStyle}
-            activeOpacity={0.3}
-            onPress={() => this.props.navigation.navigate('SignUpScreen')}>
-            <Text style={styles.TextStyle}>Login/Create An Account</Text>
-          </TouchableOpacity>
+          <Text style={styles.signUpText}>
+            Dont have an account? Create one{' '}
+            <Text
+              style={{textDecorationLine: 'underline'}}
+              onClick={() => this.props.navigation.navigate('SignUpScreen')}>
+              here
+            </Text>
+          </Text>
         </KeyboardAvoidingView>
       </View>
     );
