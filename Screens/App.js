824d5b66e787f884243f6a45e11f8f1420a8b378
Sam Joseph <sjoseph92@gmail.com>
2019-09-19 06:29:43 -0400

    added SignUpScreen to navigation stack

--- Screens/App.js
+++ Screens/App.js
@@ -1,11 +1,13 @@
 import QRScanner from './QRScanner.js';
 import AuthenticationScreen from './AuthenticationScreen.js';
+import SignUpScreen from './SignUpScreen.js'
 import VideoPlayerScreen from './VideoPlayerScreen.js';
 import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 
 const App = createSwitchNavigator(
   {
     AuthenticationScreen,
+    SignUpScreen,
     QRScanner,
     VideoPlayerScreen,
   },
