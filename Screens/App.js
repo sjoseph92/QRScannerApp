7612cab525481d1367c4ecd9e5ba0425dfee62ef
Sam Joseph <sjoseph92@gmail.com>
2019-09-20 15:08:52 -0400

    added a top navigator so that users can swipe between the QR scanner and the videos that have scanned for

--- Screens/App.js
+++ Screens/App.js
@@ -1,14 +1,35 @@
 import QRScanner from './QRScanner.js';
 import AuthenticationScreen from './AuthenticationScreen.js';
-import SignUpScreen from './SignUpScreen.js'
+import SignUpScreen from './SignUpScreen.js';
 import VideoPlayerScreen from './VideoPlayerScreen.js';
-import {createSwitchNavigator, createAppContainer} from 'react-navigation';
+import VideoListScreen from './VideoListScreen.js';
+import {
+  createSwitchNavigator,
+  createAppContainer,
+} from 'react-navigation';
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs'
+
+const QRVideoNav = createMaterialTopTabNavigator(
+  {
+    VideoList: {screen: VideoListScreen},
+    QRScanner: {screen: QRScanner},
+  },
+  {
+    initialRouteName: 'QRScanner',
+    animationEnabled: true,
+    tabBarOptions: {
+      showLabel: false,
+      showIcon: false,
+      style: {height: 0},
+    },
+  },
+);
 
 const App = createSwitchNavigator(
   {
     AuthenticationScreen,
     SignUpScreen,
-    QRScanner,
+    QRVideoNav,
     VideoPlayerScreen,
   },
   {
