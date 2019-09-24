46b2176434e710c4ea15fffa06091a764059bb6a
Sam Joseph <sjoseph92@gmail.com>
2019-09-24 18:39:05 -0400

    made minor inline style changes

--- Screens/SignUpScreen.js
+++ Screens/SignUpScreen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {
   KeyboardAvoidingView,
   Text,
-  Keyboard,
+  Dimensions,
   TextInput,
   TouchableOpacity,
   Alert,
@@ -25,8 +25,10 @@ export default class SignUpScreen extends Component {
     let {email, password, confirmPassword} = this.state;
 
     if (password !== confirmPassword) {
-      this.setState({password: ''})
-      this.setState({confirmPassword: ''})
+      this.setState({
+        password: '',
+        confirmPassword: '',
+      });
       alert('Passwords do not match!');
     } else {
       auth()
@@ -46,51 +48,55 @@ export default class SignUpScreen extends Component {
   };
 
   render() {
+    const {width} = Dimensions.get('window');
+
     return (
       <View style={styles.MainContainer}>
-        <TextInput
-          autoCapitalize="none"
-          style={styles.textInput}
-          placeholder="First Name"
-          onChangeText={firstName => this.setState({firstName})}
-          value={this.state.firstName}
-        />
-        <TextInput
-          autoCapitalize="none"
-          style={styles.textInput}
-          placeholder="Last Name"
-          onChangeText={lastName => this.setState({lastName})}
-          value={this.state.lastName}
-        />
-        <TextInput
-          autoCapitalize="none"
-          style={styles.textInput}
-          placeholder="Email"
-          onChangeText={email => this.setState({email})}
-          value={this.state.email}
-        />
-        <TextInput
-          autoCapitalize="none"
-          secureTextEntry
-          style={styles.textInput}
-          placeholder="Password"
-          onChangeText={password => this.setState({password})}
-          value={this.state.password}
-        />
-        <TextInput
-          autoCapitalize="none"
-          secureTextEntry
-          style={styles.textInput}
-          placeholder="Confirm Password"
-          onChangeText={confirmPassword => this.setState({confirmPassword})}
-          value={this.state.confirmPassword}
-        />
-        <TouchableOpacity
-          style={styles.ButtonStyle}
-          activeOpacity={0.3}
-          onPress={() => this.createUser()}>
-          <Text style={styles.TextStyle}>Create Account</Text>
-        </TouchableOpacity>
+        <KeyboardAvoidingView>
+          <TextInput
+            autoCapitalize="none"
+            style={[{width: width * 0.9}, styles.textInput]}
+            placeholder="First Name"
+            onChangeText={firstName => this.setState({firstName})}
+            value={this.state.firstName}
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={[{width: width * 0.9}, styles.textInput]}
+            placeholder="Last Name"
+            onChangeText={lastName => this.setState({lastName})}
+            value={this.state.lastName}
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={[{width: width * 0.9}, styles.textInput]}
+            placeholder="Email"
+            onChangeText={email => this.setState({email})}
+            value={this.state.email}
+          />
+          <TextInput
+            autoCapitalize="none"
+            secureTextEntry
+            style={[{width: width * 0.9}, styles.textInput]}
+            placeholder="Password"
+            onChangeText={password => this.setState({password})}
+            value={this.state.password}
+          />
+          <TextInput
+            autoCapitalize="none"
+            secureTextEntry
+            style={[{width: width * 0.9}, styles.textInput]}
+            placeholder="Confirm Password"
+            onChangeText={confirmPassword => this.setState({confirmPassword})}
+            value={this.state.confirmPassword}
+          />
+          <TouchableOpacity
+            style={[{width: width * 0.8}, styles.ButtonStyle]}
+            activeOpacity={0.3}
+            onPress={() => this.createUser()}>
+            <Text style={styles.TextStyle}>Create Account</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </View>
     );
   }
