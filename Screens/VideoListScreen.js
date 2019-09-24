2a376f710f44342d588bf3c185956218baac3755
Sam Joseph <sjoseph92@gmail.com>
2019-09-24 18:36:43 -0400

    implemented firestore function to check if user is registered, component conditionally renders based of this

--- Screens/VideoListScreen.js
+++ Screens/VideoListScreen.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SpinningWheel from './SpinningWheel.js';
 import {
   KeyboardAvoidingView,
   Text,
@@ -9,12 +10,52 @@ import {
   View,
 } from 'react-native';
 import {styles} from './styles/styles.js';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 export default class VideoListScreen extends Component {
+  state = {
+    isRegisteredUser: '',
+    scannedVideos: [],
+  };
+
+  componentDidMount() {
+    firestore()
+      .doc(`users/${auth().currentUser.uid}`)
+      .get()
+      .then(results => {
+        if (results.exists === false) {
+          this.setState({isRegisteredUser: false});
+        } else {
+          this.setState({
+            isRegisteredUser: true,
+            scannedVideos: results._data.videos,
+          });
+        }
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.MainContainer}>
-        <Text style={styles.TitleText}>QRScanner</Text>
+        {!this.state.isRegisteredUser ? (
+          <View>
+            <TouchableOpacity
+              style={styles.ButtonStyle}
+              activeOpacity={0.3}
+              onPress={() => this.props.navigation.navigate('SignUpScreen')}>
+              <Text style={styles.TextStyle}>Create an account!</Text>
+            </TouchableOpacity>
+            <SpinningWheel />
+          </View>
+        ) : (
+          <View>
+            <Text style={styles.signUpText}>Welcome</Text>
+          </View>
+        )}
       </View>
     );
   }
