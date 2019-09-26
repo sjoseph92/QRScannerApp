db3a13109bd63988be952ce5316aebbd9bab32ce
Sam Joseph <sjoseph92@gmail.com>
2019-09-26 14:51:11 -0400

    removed text input, added animated text input, added functions to update respective text

--- Screens/SignUpScreen.js
+++ Screens/SignUpScreen.js
@@ -8,19 +8,30 @@ import {
   Alert,
   View,
 } from 'react-native';
+import AnimatedTextInput from './AnimatedTextInput.js';
 import {styles} from './styles/styles.js';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 export default class SignUpScreen extends Component {
-  state = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  };
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
 
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
+      this,
+    );
+  }
   addUser = () => {
     firestore()
       .collection('users')
@@ -49,7 +60,7 @@ export default class SignUpScreen extends Component {
           });
         })
         .then(() => {
-          this.addUser()
+          this.addUser();
         })
         .then(() => {
           this.props.navigation.navigate('QRScanner');
@@ -60,56 +71,60 @@ export default class SignUpScreen extends Component {
     }
   };
 
+  handleFirstNameChange = newFirstName => this.setState({firstName: newFirstName});
+  handleLastNameChange = newLastName => this.setState({lastName: newLastName});
+  handleEmailChange = newEmail => this.setState({email: newEmail});
+  handlePasswordChange = newPassword => this.setState({password: newPassword});
+  handleConfirmPasswordChange = newConfirmPassword => this.setState({confirmPassword: newConfirmPassword});
+
   render() {
     const {width} = Dimensions.get('window');
 
     return (
       <View style={styles.MainContainer}>
         <KeyboardAvoidingView>
-          <TextInput
-            autoCapitalize="none"
-            style={[{width: width * 0.9}, styles.textInput]}
-            placeholder="First Name"
-            onChangeText={firstName => this.setState({firstName})}
-            value={this.state.firstName}
-          />
-          <TextInput
-            autoCapitalize="none"
-            style={[{width: width * 0.9}, styles.textInput]}
-            placeholder="Last Name"
-            onChangeText={lastName => this.setState({lastName})}
-            value={this.state.lastName}
-          />
-          <TextInput
-            autoCapitalize="none"
-            style={[{width: width * 0.9}, styles.textInput]}
-            placeholder="Email"
-            onChangeText={email => this.setState({email})}
-            value={this.state.email}
-          />
-          <TextInput
-            autoCapitalize="none"
-            secureTextEntry
-            style={[{width: width * 0.9}, styles.textInput]}
-            placeholder="Password"
-            onChangeText={password => this.setState({password})}
-            value={this.state.password}
-          />
-          <TextInput
-            autoCapitalize="none"
-            secureTextEntry
-            style={[{width: width * 0.9}, styles.textInput]}
-            placeholder="Confirm Password"
-            onChangeText={confirmPassword => this.setState({confirmPassword})}
-            value={this.state.confirmPassword}
-          />
+        <Text style={styles.TitleText}>QRScanner</Text>
+          <View
+            style={{
+              width: width * 0.8,
+              alignSelf: 'center',
+              paddingBottom: 10,
+            }}>
+            <AnimatedTextInput
+              label="first name"
+              value={this.state.firstName}
+              onChangeText={this.handleFirstNameChange}
+            />
+            <AnimatedTextInput
+              label="last name"
+              value={this.state.lastName}
+              onChangeText={this.handleLastNameChange}
+              yt
+            />
+            <AnimatedTextInput
+              label="email"
+              value={this.state.email}
+              onChangeText={this.handleEmailChange}
+            />
+            <AnimatedTextInput
+              label="password"
+              value={this.state.password}
+              onChangeText={this.handlePasswordChange}
+            />
+            <AnimatedTextInput
+              label="confirm password"
+              value={this.state.confirmPassword}
+              onChangeText={this.handleConfirmPasswordChange}
+            />
+          </View>
+          </KeyboardAvoidingView>
           <TouchableOpacity
             style={[{width: width * 0.8}, styles.ButtonStyle]}
             activeOpacity={0.3}
             onPress={() => this.createUser()}>
             <Text style={styles.TextStyle}>Create Account</Text>
           </TouchableOpacity>
-        </KeyboardAvoidingView>
+        
       </View>
     );
   }
